fb4d7c937f9da2b443f1e19a312730e1467907f9
Lucas Campos <lucasgcampos.contato@gmail.com>
2026-05-09 13:14:15 -0300

    Donate function

--- contracts/hello-world/src/lib.rs
+++ contracts/hello-world/src/lib.rs
@@ -1,13 +1,45 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, vec, Env, String, Vec};
+use soroban_sdk::{contract, contractevent, contractimpl, Address, Env, token};
 
 #[contract]
 pub struct Contract;
 
+#[contractevent(data_format = "single-value")]
+pub struct Donate {
+    #[topic]
+    from: Address,
+    #[topic]
+    to: Address,
+    amount: i128,
+}
+
 #[contractimpl]
 impl Contract {
-    pub fn hello(env: Env, to: String) -> Vec<String> {
-        vec![&env, String::from_str(&env, "Hello"), to]
+
+     pub fn donate(
+        env: Env,
+        from: Address,
+        to: Address,
+        token_address: Address,
+        amount: i128,    
+    ) {
+        from.require_auth();
+        
+        let token = token::Client::new(&env, &token_address);
+        token.transfer(&from, &env.current_contract_address(), &amount);
+
+        let split_amount = amount * 95 / 100;
+        token.transfer(
+            &env.current_contract_address(),
+            &to,
+            &split_amount,
+        );
+
+        Donate {
+            from,
+            to,
+            amount
+        }.publish(&env);
     }
 }
 
